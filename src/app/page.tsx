5e448f783204d94b4de5c0066d6b20a1fdbc7649
Eric Spishak-Thomas <eric@spishak-thomas.new-york.ny.us>
2025-04-21 00:00:03 -0400

    Add a 'Skip to live' button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Home() {
   const [curl, setCurl] = useState('');
   const [m3u8, setM3u8] = useState('');
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   async function play() {
     const response = await fetch('/api/parse-curl', {
@@ -17,12 +18,19 @@ export default function Home() {
     setM3u8(m3u8Url.toString());
   }
 
+  function skipToLive() {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 999999999;
+    }
+  }
+
   return (
     <>
       <h1>m3u8</h1>
       {m3u8 ? (
         <div>
-          <video src={m3u8} controls autoPlay></video>
+          <video src={m3u8} controls autoPlay ref={videoRef}></video>
+          <button onClick={skipToLive}>Skip to live</button>
         </div>
       ) : (
         <div>
