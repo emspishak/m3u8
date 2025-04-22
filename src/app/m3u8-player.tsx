aff6a410d20879e5288b8077806df5c89ca0b441
Eric Spishak-Thomas <eric@spishak-thomas.new-york.ny.us>
2025-04-22 15:24:51 -0400

    Switch to URL state.
    This puts the (base64 encoded) curl command in the URL, so links can be easily shared. This reworks the app for full server side render if there is a curl command in the URL.

---
+++ src/app/m3u8-player.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useRef } from 'react';
+import styles from './styles.module.css';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+export default function M3u8Player({
+  m3u8Url,
+}: {
+  m3u8Url: string | undefined;
+}) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  async function play(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const curlElement = form.elements.namedItem(
+      'curl'
+    ) as HTMLTextAreaElement | null;
+    const curl = curlElement?.value;
+
+    let newUrl = '/';
+
+    if (curl) {
+      newUrl = `${newUrl}?${new URLSearchParams({ c: btoa(curl) }).toString()}`;
+    }
+
+    router.push(newUrl);
+  }
+
+  function skipToLive() {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 999999999;
+    }
+  }
+
+  let newM3u8Url: string | undefined;
+  if (m3u8Url) {
+    newM3u8Url = `/api/m3u8?${new URLSearchParams({ m3u8: m3u8Url })}`;
+  }
+
+  return (
+    <div className="m-2">
+      <h1 className="text-3xl">m3u8</h1>
+      {newM3u8Url ? (
+        <div>
+          <video src={newM3u8Url} controls autoPlay ref={videoRef}></video>
+          <button className={`${styles.btn} my-1`} onClick={skipToLive}>
+            Skip to live
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={play}>
+          <label>
+            Copy as curl:
+            <textarea
+              className="border-2 block w-100 h-50"
+              name="curl"
+              defaultValue={atob(searchParams.get('c') || '')}
+            ></textarea>
+          </label>
+          <div>
+            <button className={`${styles.btn} my-1`} type="submit">
+              Play
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,17 @@
-'use client';
+import M3u8Player from './m3u8-player';
+import { toJsonObject } from 'curlconverter';
 
-import { useRef, useState } from 'react';
-import styles from './styles.module.css';
-
-export default function Home() {
-  const [curl, setCurl] = useState('');
-  const [m3u8, setM3u8] = useState('');
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  async function play() {
-    const response = await fetch('/api/parse-curl', {
-      method: 'POST',
-      body: curl,
-    });
-    const json = await response.json();
-    const m3u8Url = new URL('/api/m3u8', document.baseURI);
-    m3u8Url.searchParams.set('m3u8', json.raw_url);
-    setM3u8(m3u8Url.toString());
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ c?: string }>;
+}) {
+  let m3u8Url = undefined;
+  const curlParam = atob((await searchParams).c ?? '');
+  if (curlParam) {
+    const json = toJsonObject(curlParam);
+    m3u8Url = json.raw_url;
   }
 
-  function skipToLive() {
-    if (videoRef.current) {
-      videoRef.current.currentTime = 999999999;
-    }
-  }
-
-  return (
-    <div className="m-2">
-      <h1 className="text-3xl">m3u8</h1>
-      {m3u8 ? (
-        <div>
-          <video src={m3u8} controls autoPlay ref={videoRef}></video>
-          <button className={`${styles.btn} my-1`} onClick={skipToLive}>
-            Skip to live
-          </button>
-        </div>
-      ) : (
-        <div>
-          <label>
-            Copy as curl:
-            <textarea
-              className="border-2 block w-100 h-50"
-              value={curl}
-              onChange={(e) => setCurl(e.target.value)}
-            ></textarea>
-          </label>
-          <div>
-            <button className={`${styles.btn} my-1`} onClick={play}>
-              Play
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+  return <M3u8Player m3u8Url={m3u8Url}></M3u8Player>;
 }
